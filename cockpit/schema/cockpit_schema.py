f1160f5cd0e96f4aab21c5c1b3036be2a619e6d8
DanielSporysz <daniel_spory@o2.pl>
2020-11-30 14:16:23 +0100

    Add cockpit schema and model.

---
+++ cockpit/schema/cockpit_schema.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from fastapi_camelcase import CamelModel
+
+
+class CockpitSchema(CamelModel):
+    # don't blame me, blame the devops for weird field naming
+    idtask: Optional[int] = None
+    name: Optional[str]
+    version: Optional[int] = None
+    reservedcredits: Optional[int] = None
+    idapp: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+    def json(self):
+        json_dict = {}
+        for k, v in self.__dict__.items():
+            capitalized = ''.join(word.title() for word in k.split('_'))
+            json_dict[capitalized[0].lower() + capitalized[1:]] = v
+        return json_dict
