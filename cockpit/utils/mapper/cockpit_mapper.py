d4bc4f5d63883eee3b5a7b5771a8f010da80cd93
jeleniep <piom20@gmail.com>
2020-12-15 00:11:45 +0100

    Priority int to str and str to int

--- cockpit/utils/mapper/cockpit_mapper.py
+++ cockpit/utils/mapper/cockpit_mapper.py
@@ -10,5 +10,6 @@ def cockpit_model_to_schema(cockpit_model: CockpitModel) -> CockpitSchema:
         reserved_credits=cockpit_model.reserved_credits,
         id_app=cockpit_model.id_app,
         status=cockpit_model.status,
+        priority=str(cockpit_model.priority),
         id_user=cockpit_model.id_user
     )
